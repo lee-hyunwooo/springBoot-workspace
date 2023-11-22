d738e29b5c6ec79b5e2fc1d0ea576ce8b9df9924
lee-hyunwooo <blacktear7@naver.com>
2023-11-22 11:35:24 +0900

    m

--- spring-DB-Oracle/src/main/java/com/kh/springdb/service/ProductService.java
+++ spring-DB-Oracle/src/main/java/com/kh/springdb/service/ProductService.java
@@ -5,13 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.springdb.mapper.ProductMapper;
 import com.kh.springdb.model.Product;
 
 @Service
 public class ProductService {
-	//@Autowired
-	//public List<Product> getAllProduct() {
-	//	return "찾은 제품 모두 다 가지고 오는 메서드 추가";
-	//}
+	@Autowired
+	private ProductMapper productMapper;
+	public List<Product> getAllProduct() {
+		return productMapper.getAllProducts();
+	}
 	
 }
