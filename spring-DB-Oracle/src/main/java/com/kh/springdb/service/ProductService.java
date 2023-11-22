266afd219fbcff69b7418d20c01d10fb7d7954f0
lee-hyunwooo <143372395+lee-hyunwooo@users.noreply.github.com>
2023-11-22 11:17:19 +0900

    Add files via upload

---
+++ spring-DB-Oracle/src/main/java/com/kh/springdb/service/ProductService.java
@@ -0,0 +1,17 @@
+package com.kh.springdb.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.springdb.model.Product;
+
+@Service
+public class ProductService {
+	//@Autowired
+	//public List<Product> getAllProduct() {
+	//	return "찾은 제품 모두 다 가지고 오는 메서드 추가";
+	//}
+	
+}
