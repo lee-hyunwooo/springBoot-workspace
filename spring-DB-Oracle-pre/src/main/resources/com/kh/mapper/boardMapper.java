573780a96b98d3a5902fd3e0914f5b8522f69d93
lee-hyunwooo <blacktear7@naver.com>
2023-11-29 17:36:35 +0900

    spring

--- spring-DB-Oracle-pre/src/main/resources/com/kh/mapper/boardMapper.java
+++
@@ -1,6 +0,0 @@
-package com.kh.mapper;
-
-public interface boardMapper {
-	List<Board> getAllBoards();
-
-}
---
+++ spring-JPA-Board/src/main/java/com/kh/board/controller/BoardController.java
@@ -0,0 +1,79 @@
+package com.kh.board.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.board.service.BoardService;
+import com.kh.board.vo.Board;
+
+@Controller
+@RequestMapping("/boards")
+public class BoardController {
+	
+	private final BoardService boardService;
+	
+	@Autowired
+	public BoardController(BoardService boardService) {
+		this.boardService = boardService;
+	}
+	
+	@GetMapping
+	public String getAllBoards(Model model) {
+		List<Board> boards = boardService.getAllBoards();
+		model.addAttribute("boards", boards);
+		return "board_list";
+	}
+	
+	//게시물 상세보기 컨트롤러
+	@GetMapping("/details/{boardId}")
+	public String getBoardDetails(@PathVariable Long boardId, Model model) {
+		Optional<Board> board = boardService.getBoardById(boardId);
+		board.ifPresent(value -> model.addAttribute("board",value));
+		return "board_detail";
+	}
+	
+	@GetMapping("/new")
+	public String displayBoardForm(Model model) {
+		model.addAttribute("board", new Board());
+		return "board_form";
+	}
+	
+	@PostMapping("/save")
+	public String saveBoard(@ModelAttribute Board board) {
+		boardService.saveBoard(board);
+		return "redirect:/boards";
+	}
+	//수정 폼 제공
+	@GetMapping("/update/{boardId}")
+	public String updateboard(@PathVariable Long boardId, Model model) {
+		Optional<Board> board = boardService.getBoardById(boardId);
+		board.ifPresent(value -> model. addAttribute("board",value));
+		return "board_update";
+	}
+	//수정 기능
+	@PostMapping("/update/{boardId}")
+	public String updateBoard(@ModelAttribute Board board) {
+		boardService.saveBoard(board);
+		return "redirect:/boards";
+	}
+	@GetMapping("/delete/{boardId}")
+	public String deleteboard(@PathVariable Long boardId) {
+		boardService.deleteBoardById(boardId);
+		return "redirect:/boards";
+	}
+	//모두삭제
+	@GetMapping("/deleteAll")
+	public String deleteAllBoards() {
+		boardService.deleteAllBoards();
+		return "redirect:/boards";
+	}
+}
